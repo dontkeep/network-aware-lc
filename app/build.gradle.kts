931aa92bca62e0ddf1df21fd9e49aaf454d02a77
dontkeep <alsdiaryy@gmail.com>
2025-01-01 21:40:59 +0700

    edited gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("com.github.dontkeep:network-aware-lc:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -58,4 +57,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(project(":networkawarelazycolumns"))
 }
